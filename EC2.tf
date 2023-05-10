6d4a0152cc7ec7787ca2c6d771dca80ed1034673
Ola Oruku <olaoruku007@gmail.com>
2023-05-10 16:22:06 -0400

    Create provider.tf and variables.tf, also modify EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,15 +1,11 @@
-provider "aws" {
-  region = "us-east-2"
-}
-
 data "aws_key_pair" "Dev_KP_OH" {
   key_name           = "Dev_KP_OH"
   include_public_key = true
 }
 
 resource "aws_instance" "instance-pro" {
-  ami           =  "ami-0a04068a95e6a1cde" 
-  instance_type = "t2.medium"
+  ami           =  var.image_id
+  instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.pro-sg.id]
   key_name = data.aws_key_pair.Dev_KP_OH.key_name
 
@@ -35,28 +31,26 @@ resource "aws_instance" "instance-pro" {
 resource "aws_security_group" "pro-sg" {
   name = "terraform-pro-sg"
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = var.HTTP_port
+    to_port     = var.HTTP_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = var.SSH_port
+    to_port     = var.SSH_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
 
    egress {
-    from_port   = 0
-    to_port     = 0
+    from_port   = var.ALL_port
+    to_port     = var.ALL_port
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-
 }
 
 
