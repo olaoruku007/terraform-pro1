090b39bc999129fe157717632b7bbb1423672712
Ola Oruku <olaoruku007@gmail.com>
2023-05-19 08:02:06 -0400

    install syntax highlighting and autocompletion for terraform.

--- ASG-ALB/asg-alb.tf
+++ ASG-ALB/asg-alb.tf
@@ -12,24 +12,31 @@ data "aws_subnets" "default" {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
+
+  filter {
+    name   = "map-public-ip-on-launch"
+    values = ["true"]
+  }
+  # ids = data.aws_subnet.default.*.id #ids attribute is added to fetch the subnet IDs and store them in the "ids" variable for later use.
 }
+
 
 data "aws_security_group" "default_vpc_sg" {
   vpc_id = data.aws_vpc.default.id
 
   filter {
-    name = "group-name"
+    name   = "group-name"
     values = ["default"]
   }
 }
 
 resource "aws_launch_configuration" "instance-lc-asg" {
-  image_id                    = var.image_id
-  instance_type               = var.instance_type
-  security_groups             = [aws_security_group.pro-sg.id]
-  key_name                    = data.aws_key_pair.Dev_KP.key_name
+  image_id        = var.image_id
+  instance_type   = var.instance_type
+  security_groups = [aws_security_group.pro-sg.id]
+  key_name        = data.aws_key_pair.Dev_KP.key_name
   #associate_public_ip_address = true
-  user_data                   = <<-EOF
+  user_data = <<-EOF
               #!/bin/bash
               dnf update -y
               dnf install httpd -y
@@ -39,7 +46,8 @@ resource "aws_launch_configuration" "instance-lc-asg" {
               echo '<center><h1>Hello, World! This is Wakandom Lomo. This Amazon EC2 instance is located in Availability Zone: AZID </h1></center>' > /var/www/html/index.txt
               sed "s/AZID/$EC2AZ/" /var/www/html/index.txt > /var/www/html/index.html
               systemctl restart httpd
-              
+              firewall-cmd --permanent --add-port=80/tcp && firewall-cmd --reload
+              systemctl restart firewalld
               EOF
 
   # Required when using a launch configuration with an ASG.
@@ -51,12 +59,12 @@ resource "aws_launch_configuration" "instance-lc-asg" {
 resource "aws_autoscaling_group" "pro-asg" {
   launch_configuration = aws_launch_configuration.instance-lc-asg.name
 
-  min_size = 5
-  max_size = 5
+  min_size = 12
+  max_size = 12
 
-  vpc_zone_identifier =  data.aws_subnets.default.ids
-               
-  
+  vpc_zone_identifier = data.aws_subnets.default.ids
+
+
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
 
@@ -76,9 +84,9 @@ resource "aws_security_group" "pro-sg" {
   name   = "terraform-pro-sg"
   vpc_id = data.aws_vpc.default.id
   ingress {
-    from_port   = var.HTTP_port
-    to_port     = var.HTTP_port
-    protocol    = "tcp"
+    from_port       = var.HTTP_port
+    to_port         = var.HTTP_port
+    protocol        = "tcp"
     security_groups = [aws_security_group.alb.id]
   }
   ingress {
@@ -99,10 +107,9 @@ resource "aws_security_group" "pro-sg" {
 resource "aws_lb" "terraform-alb" {
   name               = "terraform-pro-asg-alb"
   load_balancer_type = "application"
-  subnets            =  data.aws_subnets.default.ids
-    
+  subnets            = data.aws_subnets.default.ids
 
-  security_groups    = [aws_security_group.alb.id, data.aws_security_group.default_vpc_sg.id]
+  security_groups = [aws_security_group.alb.id, data.aws_security_group.default_vpc_sg.id]
 }
 
 resource "aws_lb_listener" "http" {
@@ -150,12 +157,16 @@ resource "aws_lb_target_group" "asg" {
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.default.id
 
+  lifecycle {
+    create_before_destroy = true
+  }
+
   health_check {
     path                = "/"
     protocol            = "HTTP"
     matcher             = "200"
-    interval            = 30
-    timeout             = 5
+    interval            = 15
+    timeout             = 10
     healthy_threshold   = 3
     unhealthy_threshold = 2
   }
@@ -183,7 +194,7 @@ output "alb_dns_name" {
 }
 
 output "instance_ips" {
-  value =  aws_launch_configuration.instance-lc-asg.*.associate_public_ip_address
-    
+  value = aws_launch_configuration.instance-lc-asg.*.associate_public_ip_address
+
   description = "The public IP addresses of the web server"
 }
